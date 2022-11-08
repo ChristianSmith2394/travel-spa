d1dd768ffc70d1a5e0cb8d1328e8f1058da6ebef
Kyle VandeWiele <kylevandewiele97@gmail.com>
2022-11-07 23:59:06 -0600

    padding for form

--- src/component/TripForm.js
+++ src/component/TripForm.js
@@ -6,22 +6,22 @@ function TripForm() {
     <div className="container justify-content-center">
       <div className="row mt-5">
         <form className="schedule__form">
-          <div className="col">
-            <input type="text" class="form-control" placeholder="Destination" />
+          <div className="col px-2">
+            <input type="text" className="form-control" placeholder="Destination" />
           </div>
-          <div className="col">
-            <input type="text" class="form-control" placeholder="Departure date" />
+          <div className="col px-2">
+            <input type="text" className="form-control" placeholder="Departure date" />
           </div>
-          <div className="col">
-            <input type="text" class="form-control" placeholder="Return date" />
+          <div className="col px-2">
+            <input type="text" className="form-control" placeholder="Return date" />
           </div>
-          <div className="col">
-            <input type="text" class="form-control" placeholder="Number of people" />
+          <div className="col px-2">
+            <input type="text" className="form-control" placeholder="Number of people" />
           </div>
         </form>
 
         <div className="btn mt-3">
-          <button type="button" className="btn btn-light">
+          <button type="submit" className="btn btn-light">
             Search
           </button>
         </div>
