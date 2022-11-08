77fb5dc2a809f1f1783636b8596454aa223d1de6
Kyle VandeWiele <kylevandewiele97@gmail.com>
2022-11-08 16:46:39 -0600

    cleaned up form- added floating labels, fixed formbg image, added form container over formbg, changed button color

--- src/component/TripForm.js
+++ src/component/TripForm.js
@@ -1,46 +1,65 @@
 import React from "react";
 import "./TripForm.css";
-import bg from "../assets/formbg.png";
 
 function TripForm() {
   return (
-    <div className="container">
-      <div className="row pt-5">
-        <form className="schedule__form">
-          <div className="col px-2">
+    <div className="formBG">
+      <div className="container border rounded" id="form">
+        <div className="row p-3 pt-5">
+          <div className="col px-2 form-floating">
             <input
               type="text"
               className="form-control"
+              id="departureInput"
+              placeholder="Departure"
+            />
+            <label for="departureInput">Departure</label>
+          </div>
+          <div className="col px-2 form-floating">
+            <input
+              type="text"
+              className="form-control"
+              id="destinationInput"
               placeholder="Destination"
             />
+            <label for="destinationInput">Destination</label>
           </div>
-          <div className="col px-2">
+          <div className="col px-2 form-floating">
+            <input
+              type="number"
+              className="form-control"
+              id="passengersInput"
+              placeholder="Passengers"
+            />
+            <label for="passengersInput">Passengers</label>
+          </div>
+        </div>
+        <div className="row p-3">
+          <div className="col px-2 form-floating">
             <input
               type="date"
               className="form-control"
-              placeholder="Departure date"
+              id="departureDate"
+              placeholder="Departure Date"
             />
+            <label for="departureDate">Departure Date</label>
           </div>
-          <div className="col px-2">
+          <div className="col px-2 form-floating">
             <input
               type="date"
-              className="form-control"
-              placeholder="Return date"
+              className="form-control form-floating"
+              id="returnDate"
+              placeholder="Return Date"
             />
+            <label for="returnDate">Return Date</label>
           </div>
-          <div className="col px-2">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Number of people"
-            />
+          <div className="row p-3">
+            <div className="btn mt-3">
+              <button type="submit" className="btn btn-outline-success">
+                Search
+              </button>
+            </div>
           </div>
-        </form>
-
-        <div className="btn mt-3">
-          <button type="submit" className="btn btn-light">
-            Search
-          </button>
         </div>
       </div>
     </div>
