3220afefeba02876d965b3311fd5d52f3596fd33
Christian <christiansmith2394@gmail.com>
2022-11-11 23:02:22 -0500

    Added routing for nav bar selections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import Flights from "./pages/Flights";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/flights" element={<Flights />} />
+        {/* <Route path="/cars" element={<Cars />} /> */}
+        {/* <Route path="/hotels" element={<Hotels />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Flights.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Flights() {
+  return <div>Flights</div>;
+}
+
+export default Flights;
