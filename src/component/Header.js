e8a87629338d866c28592e8586d37bdb5dbfca68
Kyle VandeWiele <kylevandewiele97@gmail.com>
2022-11-13 21:41:47 -0600

    changed map page to results and resized

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,32 +5,30 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="jumbotron jumbotron-fluid mb-0 pt-3 pb-3 rounded-0 ">
-      <div className="container-fluid">
-        <div className="row">
-          <a className="col-3 logo" href="/">
-            <img src={logo} alt="logo" />
-          </a>
-          <div className="col"></div>
-          <div className="col-4">
-            <nav className="navbar">
-              <li className="nav-item">
-                <a className="navbar-brand" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="navbar-brand" href="/map">
-                  Map
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="navbar-brand" href="/login">
-                  Login
-                </a>
-              </li>
-            </nav>
-          </div>
+    <header className="jumbotron jumbotron-fluid mb-0 pt-3 pb-3 rounded-0">
+      <div className="row mx-0">
+        <a className="col-3 logo" href="/">
+          <img src={logo} alt="logo" />
+        </a>
+        <div className="col"></div>
+        <div className="col-3">
+          <nav className="navbar">
+            <li className="nav-item">
+              <a className="navbar-brand" href="/">
+                Home
+              </a>
+            </li>
+            {/* <li className="nav-item">
+              <a className="navbar-brand" href="/results">
+                Results
+              </a>
+            </li> */}
+            <li className="nav-item">
+              <a className="navbar-brand" href="/login">
+                Login
+              </a>
+            </li>
+          </nav>
         </div>
       </div>
     </header>
--- src/component/Map.js
+++ src/component/Map.js
@@ -6,22 +6,30 @@ export default function ShowMap() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyC_lcR30C7ExXrZgEJGqOjKZvQ4dySnp88",
   });
-
+  
   if (!isLoaded) return <div>Loading...</div>;
   return <Map />;
 }
 
 function Map() {
+  const mapOptions = {
+    fullscreenControl: false,
+    streetViewControl: false,
+    zoomControl: false,
+    mapTypeControl: false,
+    scrollwheel: false,
+    draggable: false,
+  };
   const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
-
   return (
     <>
       <GoogleMap
         zoom={3}
         center={center}
         mapContainerClassName="map-container"
+        options={mapOptions}
       >
-        <Marker position={{lat: 44, lng: -80}} />
+        <Marker position={{ lat: 44, lng: -80 }} />
       </GoogleMap>
     </>
   );
