4befa37968fac2edb2904f1100fe8047847d270e
Kyle VandeWiele <kylevandewiele97@gmail.com>
2022-11-14 23:31:40 -0600

    navbar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,37 +1,38 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import "./Header.css";
-import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="jumbotron jumbotron-fluid mb-0 pt-3 pb-3 rounded-0">
-      <div className="row mx-0">
-        <a className="col-3 logo" href="/">
+    <nav class="navbar navbar-light navbar-expand-sm">
+      <div class="container">
+        <a class="col-3 navbar-brand" href="#">
           <img src={logo} alt="logo" />
         </a>
-        <div className="col"></div>
-        <div className="col-3">
-          <nav className="navbar">
-            <li className="nav-item">
-              <a className="navbar-brand" href="/">
-                Home
-              </a>
-            </li>
-            {/* <li className="nav-item">
-              <a className="navbar-brand" href="/results">
-                Results
-              </a>
-            </li> */}
-            <li className="nav-item">
-              <a className="navbar-brand" href="/login">
-                Login
-              </a>
-            </li>
-          </nav>
-        </div>
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/results">
+              Results
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">
+              About
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">
+              Login
+            </a>
+          </li>
+        </ul>
       </div>
-    </header>
+    </nav>
   );
 }
 
