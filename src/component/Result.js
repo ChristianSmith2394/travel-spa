71bc2c0e0e748723e88659bf50bd46c017d70993
Kyle VandeWiele <kylevandewiele97@gmail.com>
2022-11-20 17:05:18 -0600

    added modal for more information

--- src/component/Result.js
+++ src/component/Result.js
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Result.css";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 import sampleResults from "../data/sampleResults.json";
 import Map from "./Map";
+import { Modal, Button, Form, Container } from "react-bootstrap";
+
+function FlightInfo(result) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button className="nextButton btn-ckt" onClick={handleShow}>
+        Details
+      </Button>
+
+      <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header>
+          <Modal.Title>Title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+        <Map departCode={result.departCode} arriveCode={result.arriveCode} />
+        </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="ckt" type="submit" onClick={handleClose}>
+            Book
+          </Button>
+
+          <Button variant="" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 
 function Result(departCode, arriveCode, departDate, returnDate, passengers) {
-  // const [results, setResults] = React.useState([]);
 
-  // console.log("departCode: " + departCode);
-  // console.log("arriveCode: " + arriveCode);
-  // console.log("passengers: " + passengers);
 
-  //   console.log(url);
+// const handleOnClick = (result) => {
+//     console.log(result);
 
-  // GET LIST RESULTS FROM TRIPFORM AND BUILD OUT FLIGHT
-  // COMPONENT TO DISPLAY RESULTS
-
-  //   fetch("https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode="+departCode+"&destinationLocationCode="+arriveCode+"&departureDate=2021-11-01&adults="+passengers+"&max=2")
-  // .then((res) => res.json())
-  // .then(
-  //   (result) => {
-  //     console.log(result);
-  //     setResults(result);
-  //     });
-
-const handleOnClick = (result) => {
-    console.log(result);
-
-    <Map departCode={result.departCode} arriveCode={result.arriveCode} />;
-  };
+//     
+//   };
 
   return sampleResults.map((result) => (
     <Row className="result-card mx-auto">
@@ -70,7 +87,10 @@ const handleOnClick = (result) => {
             <Card.Subtitle className="mb-2 text-muted">
               {result.price}
             </Card.Subtitle>
-            <Button variant="btn btn-ckt" onClick={handleOnClick(result)}>Details</Button>
+            <FlightInfo 
+            departCode={result.departCode}  
+            arriveCode={result.arriveCode}
+            />
           </Card.Body>
         </Card>
       </Col>
