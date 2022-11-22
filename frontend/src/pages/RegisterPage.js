1a7c9746455837491e981f38e385055a066e7450
Christian <christiansmith2394@gmail.com>
2022-11-22 17:59:09 -0500

    Working on registration page styling

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./RegisterPage.css";
+import logo from "../assets/logo.png";
 
 function RegisterPage() {
   return (
     <div className="registration">
-      <div className="">Left side</div>
+      <img src={logo} alt="CKT logo" className="reg-logo" />
+      <h1>CKT, find all your travel needs!</h1>
+      <form className="loginForm">
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Email"
+          name="email"
+          required
+        ></input>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+        ></input>
+      </form>
+      <button className="registerButton">Register!</button>
+      <div className="create-new"></div>
     </div>
   );
 }
