ee4d3770188511b99c0245103684300dc3606aca
Christian <christiansmith2394@gmail.com>
2022-11-22 18:14:02 -0500

    Added the header to the reg page

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,57 +1,60 @@
 import React from "react";
 import "./RegisterPage.css";
 import logo from "../assets/logo.png";
+import Header from "../component/Header";
 
 function RegisterPage() {
   return (
-    <div className="registration">
-      <img src={logo} alt="CKT logo" className="reg-logo" />
-      <h1>CKT, find all your travel needs!</h1>
-      <form className="registerForm">
-        <input
-          className="registerInput"
-          type="text"
-          placeholder="First name"
-          name="fName"
-          required
-        />
-        <input
-          className="registerInput"
-          type="text"
-          placeholder="Last name"
-          name="lName"
-          required
-        />
-        <input
-          className="registerInput"
-          type="email"
-          placeholder="Email"
-          name="email"
-          required
-        />
-        <input
-          className="registerInput"
-          type="password"
-          placeholder="Password"
-          name="password"
-          required
-        />
-        <input
-          className="registerInput"
-          type="password"
-          placeholder="Confirm Password"
-          name="password"
-          required
-        />
-        <input
-          className="registerInput"
-          type="text"
-          placeholder="Home airport (3 letter airport code)"
-          name="airport"
-        />
-      </form>
-      <button className="registerButton">Register!</button>
-      <div className="create-new"></div>
+    <div>
+      <Header />
+      <div className="registration">
+        <img src={logo} alt="CKT logo" className="reg-logo" />
+        <h1>CKT, find all your travel needs!</h1>
+        <form className="registerForm">
+          <input
+            className="registerInput"
+            type="text"
+            placeholder="First name"
+            name="fName"
+            required
+          />
+          <input
+            className="registerInput"
+            type="text"
+            placeholder="Last name"
+            name="lName"
+            required
+          />
+          <input
+            className="registerInput"
+            type="email"
+            placeholder="Email"
+            name="email"
+            required
+          />
+          <input
+            className="registerInput"
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+          />
+          <input
+            className="registerInput"
+            type="password"
+            placeholder="Confirm Password"
+            name="password"
+            required
+          />
+          <input
+            className="registerInput"
+            type="text"
+            placeholder="Home airport (3 letter airport code)"
+            name="airport"
+          />
+        </form>
+        <button className="registerButton">Register!</button>
+      </div>
     </div>
   );
 }
