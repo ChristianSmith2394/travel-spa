f863db0ee164e2e007494b391f05018137b9d95d
Christian <christiansmith2394@gmail.com>
2022-11-22 14:29:01 -0500

    folder cleanup

--- frontend/src/component/Register.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Register.css";
-
-function Register() {
-  return <div>Register</div>;
-}
-
-export default Register;
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./RegisterPage.css";
-// import Header from "../component/Header";
-// import Footer from "../component/Footer";
 
 function RegisterPage() {
   return (
     <div className="registration">
-      <div className="left-side">Left side</div>
-      <div className="right-side">Right side</div>
+      <div className="">Left side</div>
     </div>
   );
 }
